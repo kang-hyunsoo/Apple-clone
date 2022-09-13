e8330f4e463f9491221a859e17b254fefc3beafc
Hyunsoo Kang <hskang9308@gmail.com>
2022-09-13 13:38:45 +0900

    feat: 비디오 인터랙션 구현

--- js/pracMain.js
+++ js/pracMain.js
@@ -20,9 +20,14 @@
                 messageA: document.querySelector('#scroll-section-0 .main-message.a'),
                 messageB: document.querySelector('#scroll-section-0 .main-message.b'),
                 messageC: document.querySelector('#scroll-section-0 .main-message.c'),
-                messageD: document.querySelector('#scroll-section-0 .main-message.d')
+                messageD: document.querySelector('#scroll-section-0 .main-message.d'),
+                canvas: document.querySelector('#video-canvas-0'),
+                context: document.querySelector('#video-canvas-0').getContext('2d'),
+                videoImages: []
             },
             values: {
+                videoImageCount: 300,
+                imageSequence: [0, 299],
                 messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
                 messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4 }],
                 messageC_opacity_in: [0, 1, { start: 0.5, end: 0.6 }],
@@ -85,7 +90,7 @@
                 pinB_opacity_out: [1, 0, { start: 0.58, end: 0.63 }],
                 pinC_opacity_out: [1, 0, { start: 0.85, end: 0.9 }]
             }
-        }  ,
+        },
         {
             // 3
             type: 'sticky',
@@ -100,6 +105,17 @@
             }
         }
     ];
+
+    function setCanvasImages() {
+        let imgElem;
+        for (let i = 0; i < sceneInfo[0].values.videoImageCount; i++) {
+            // imgElem = document.createElement('image')
+            imgElem = new Image()
+            imgElem.src = `./video/001/IMG_${6726 + i}.JPG`;
+            sceneInfo[0].objs.videoImages.push(imgElem);
+        }
+    }
+    setCanvasImages()
 
     function setLayout() {
         // 각 스크롤 섹션의 높이 셋팅
@@ -165,6 +181,10 @@
         switch (currentScene) {
             case 0:
                 // console.log('0 play');
+                let sequence = Math.round(calcValues(values.imageSequence, currentYOffset));
+                console.log(sequence);
+                objs.context.drawImage(objs.videoImages[sequence], 0, 0);
+
                 if (scrollRatio <= 0.22) {
                     // in
                     objs.messageA.style.opacity = calcValues(values.messageA_opacity_in, currentYOffset);
