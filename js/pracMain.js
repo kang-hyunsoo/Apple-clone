1ad36418859e8bd620f2c3d2047cab31f5a2ef76
Hyunsoo Kang <hskang9308@gmail.com>
2022-10-13 14:09:07 +0900

    feat: 이미지 블렌딩 후 캔버스 스케일 조정하기

--- js/pracMain.js
+++ js/pracMain.js
@@ -116,8 +116,10 @@
                 images: []
             },
             values: {
-                rect1X: [ 0, 0, {start: 0, end: 0} ],
-                rect2X: [ 0, 0, {start: 0, end: 0} ],
+                rect1X: [ 0, 0, { start: 0, end: 0 } ],
+                rect2X: [ 0, 0, { start: 0, end: 0 } ],
+                blendHeight: [0, 0, { start: 0, end: 0 }],
+                canvas_scale:[0, 0, { start: 0, end: 0 }],
                 rectStartY: 0,
 
             }
@@ -432,14 +434,37 @@
                 if (scrollRatio < values.rect1X[2].end ) {
                     step = 1;
                     objs.canvas.classList.remove('sticky')
-                    console.log('캔버스 닿기 전')
+                    // console.log('캔버스 닿기 전')
                 }
                 else  {
                     step = 2;
-                    objs.canvas.classList.add('sticky')
-                    objs.canvas.style.top = `${-(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2}px`
-                    console.log('캔버스 닿은 후')
+
                     // 이미지 블랜드
+                    values.blendHeight[0] = 0;
+                    values.blendHeight[1] = objs.canvas.height;
+                    values.blendHeight[2].start = values.rect1X[2].end;
+                    values.blendHeight[2].end = values.blendHeight[2].start + 0.2;
+
+                    const blendHeight = calcValues(values.blendHeight, currentYOffset);
+
+                    objs.context.drawImage(objs.images[1],
+                        0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight,
+                        0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight
+                    );
+
+                    objs.canvas.classList.add('sticky');
+                    objs.canvas.style.top = `${-(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2}px`
+                    // console.log('캔버스 닿은 후')
+
+                    if (scrollRatio > values.blendHeight[2].end) {
+                        console.log('축소시작')
+                        values.canvas_scale[0] = canvasScaleRatio;
+                        values.canvas_scale[1] = document.body.offsetWidth / (1.5 * objs.canvas.width);
+                        values.canvas_scale[2].start = values.blendHeight[2].end;
+                        values.canvas_scale[2].end = values.canvas_scale[2].start + 0.2;
+
+                        objs.canvas.style.transform = `scale(${calcValues(values.canvas_scale, currentYOffset)})`
+                    }
 
             }
                 break;
